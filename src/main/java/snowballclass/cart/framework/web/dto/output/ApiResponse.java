42f1d9caaec8d95cde30cf05e453308924de68f1
bhyunnie <dev.bhyunnie@gmail.com>
2025-02-23 18:28:23 +0900

    feat: 공통 응답 레코드 추가

---
+++ src/main/java/snowballclass/cart/framework/web/dto/output/ApiResponse.java
@@ -0,0 +1,22 @@
+package snowballclass.cart.framework.web.dto.output;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public record ApiResponse<T>(
+        HttpStatus status,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(HttpStatus.OK, null, data);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(HttpStatus.CREATED, null, data);
+    }
+
+    public static <T> ApiResponse<T> accepted(T data) {
+        return new ApiResponse<>(HttpStatus.ACCEPTED, null, data);
+    }
+}
