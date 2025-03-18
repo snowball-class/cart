503145afde100da3b533790f213207e0cce12c73
bhyunnie <dev.bhyunnie@gmail.com>
2025-03-18 16:00:30 +0900

    fix: 필요 없는 queryMethod 삭제

--- src/main/java/snowballclass/cart/framework/adapter/jpa/CartRepository.java
+++ src/main/java/snowballclass/cart/framework/adapter/jpa/CartRepository.java
@@ -3,9 +3,5 @@ package snowballclass.cart.framework.adapter.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import snowballclass.cart.domain.Cart;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findByMemberUUID(UUID memberUUID);
 }
